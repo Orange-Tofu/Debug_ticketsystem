339f01bf9cfd01a4d7a299ebaeabad124ea6d71f
Astraxx04 <gagan200254@gmail.com>
2023-03-04 00:27:48 +0530

    incomplete add show and add venue buttons

--- static/js/check.js
+++ static/js/check.js
@@ -1,4 +1,4 @@
-//Venue creation
+
 const venuesContainer = document.getElementById("venues-container");
 
 const venueData = [
@@ -6,7 +6,13 @@ const venueData = [
 	{ name: "Venue 2", time: 25, },
 	{ name: "Venue 3", time: 40, }
 ];
+const cardData = [
+    { name: "Show 1", time: 30, },
+    { name: "Show 2", time: 25, },
+    { name: "Show 3", time: 40, }
+];
 const numVenueToPrint = 3;
+const numCardsToPrint = 5;
 
 for (let i = 0; i < numVenueToPrint; i++) {
 	// Create a new card element
@@ -18,25 +24,11 @@ for (let i = 0; i < numVenueToPrint; i++) {
 	const vcardDataItem = venueData[vcardDataIndex];
 	vcard.innerHTML = `
 		<h1>${vcardDataItem.name}</h1>
-        <button onclick='createShows()'>Click me</button>
 	`;
 
-	// Add the card element to the card container
-	venuesContainer.appendChild(vcard);
-}
-const vcard = document.createElement("div");
-vcard.classList.add("vvcard");
-vcard.innerHTML = `<button class="venueadd_button" id="venueadd_button"><img class="add-venue-img" id="add-venue-img" src="../images/plus_icon.png" alt="Add a new Show"></button>`;
-// Add the card element to the card container
-venuesContainer.appendChild(vcard);
-
-
-
-
-
-function createShows() {
-    //Show creation
-    const showsContainer = document.getElementById("shows-container");
+    const x=document.createElement("div");
+    x.classList.add("shows");
+    x.id="shows-container";
 
     const cardData = [
         { name: "Show 1", time: 30, },
@@ -45,11 +37,15 @@ function createShows() {
     ];
     const numCardsToPrint = 5;
 
+
+    const showsContainer = x;
+    console.log(showsContainer);
+
+
     for (let i = 0; i < numCardsToPrint; i++) {
         // Create a new card element
         const card = document.createElement("div");
         card.classList.add("card");
-        card.id="card-cont";
 
         // Add the card data to the element
         const cardDataIndex = i % cardData.length; // Use modulo to cycle through the data
@@ -59,7 +55,7 @@ function createShows() {
             <p>Timings: ${cardDataItem.time}</p>
             <button class="actions_button" id="actions_button">Actions</button>
         `;
-
+        console.log(card)
         // Add the card element to the card container
         showsContainer.appendChild(card);
     }
@@ -68,6 +64,12 @@ function createShows() {
     card.innerHTML = `<button class="showadd_button" id="showadd_button"><img class="add-show-img" id="add-show-img" src="../images/plus_icon.png" alt="Add a new Show"></button>`;
     // Add the card element to the card container
     showsContainer.appendChild(card);
+
+	// Add the card element to the card container
+	venuesContainer.appendChild(vcard);
 }
-
-
+const vcard = document.createElement("div");
+vcard.classList.add("vcard");
+vcard.innerHTML = `<button class="venueadd_button" id="venueadd_button"><img class="add-venue-img" id="add-venue-img" src="../images/plus_icon.png" alt="Add a new Show"></button>`;
+// Add the card element to the card container
+venuesContainer.appendChild(vcard);
