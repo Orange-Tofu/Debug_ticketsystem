56a19ac5bbdae80a8bd44d5ad2dedd065f402979
Astraxx04 <gagan200254@gmail.com>
2023-03-06 15:26:36 +0530

    Database creation

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ticketshow.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ticket_show.db'
 
 db = SQLAlchemy(app)
 
-
-class Admin(db.Model):
+class Admins(db.Model):
     admin_id = db.Column(db.Integer(), primary_key = True)
     password = db.Column(db.String(20), nullable = False)
 
-class User(db.Model):
+class Users(db.Model):
     user_id = db.Column(db.Integer(), primary_key = True)
     password = db.Column(db.String(20), nullable = False)
     usr_name = db.Column(db.String(30), nullable = False)
@@ -25,15 +23,15 @@ class Venues(db.Model):
 class Shows(db.Model):
     show_id = db.Column(db.Integer(), primary_key = True)
     show_name = db.Column(db.String(50), nullable = False)
-    show_time = db.Column(db.Time(), nullable = False)
+    show_time = db.Column(db.DateTime(), nullable = False)
     show_tag = db.Column(db.String(50), nullable = False)
     show_rating = db.Column(db.Integer(), nullable = False)
     show_price = db.Column(db.Integer(), nullable = False)
 
 class Bookings(db.Model):
     booking_id = db.Column(db.Integer(), primary_key = True)
-    buser_id = db.Column(db.Integer(), db.ForeignKey('User.user_id'))
-    bvenue_id = db.Column(db.Integer(), db.ForeignKey('Venues.venue_id'))
-    bshow_id = db.Column(db.Integer(), db.ForeignKey('Shows.show_id'))
+    buser_id = db.Column(db.Integer(), db.ForeignKey('users.user_id'))
+    bvenue_id = db.Column(db.Integer(), db.ForeignKey('venues.venue_id'))
+    bshow_id = db.Column(db.Integer(), db.ForeignKey('shows.show_id'))
     num_tickets = db.Column(db.Integer(), nullable = False)
     total_price = db.Column(db.Integer(), nullable = False)
