45021f0d8ab7d1c1ed0e13f0aded08cb1daef21b
Astraxx04 <gagan200254@gmail.com>
2023-03-10 17:08:02 +0530

    Friday Updates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 app = Flask(__name__, template_folder='templates')
 app.secret_key = 'super secret key'
@@ -26,6 +27,9 @@ class Users(db.Model):
 class Venues(db.Model):
     venue_id = db.Column(db.Integer(), primary_key = True)
     venue_name = db.Column(db.String(50), nullable = False)
+    venue_place = db.Column(db.String(50), nullable = False)
+    venue_location = db.Column(db.String(50), nullable = False)
+    venue_capacity = db.Column(db.Integer(), nullable = False)
     shows = db.relationship("Shows")
 
     def __repr__(self):
@@ -70,6 +74,48 @@ def user_registeration():
             print(user_name, user_password)
             return "user registered"
     return render_template('registeration.html')
+
+
+
+@app.route('/newshow', methods =["GET", "POST"])
+def new_show():
+    if request.method == 'POST' :
+        new_showname = request.form.get('new_showname')
+        new_showratings = request.form.get('new_showratings')
+        new_showtime = request.form.get('new_showtime')
+        new_showtag = request.form.get('new_showtag')
+        new_showprice = request.form.get('new_showprice')
+        print(new_showtime)
+        # show = Shows(show_name=new_showname, show_tag=new_showtag, show_rating=new_showratings, show_price=new_showprice, svenue_id=1)
+        # db.session.add(show)
+        # db.session.commit()
+        # print(new_showname, new_showratings, new_showtime, new_showtag, new_showprice)
+        return "show registered"
+    return render_template('new_show.html')
+
+
+
+@app.route('/newvenue', methods =["GET", "POST"])
+def new_venue():
+    if request.method == 'POST' :
+        new_venuename = request.form.get('new_venuename')
+        new_venueloc = request.form.get('new_venueloc')
+        new_venueplace = request.form.get('new_venueplace')
+        new_venuecap = request.form.get('new_venuecap')
+        venue = Venues(venue_name=new_venuename, venue_place=new_venueplace, venue_location=new_venueloc, venue_capacity=new_venuecap)
+        db.session.add(show)
+        db.session.commit()
+        print(new_venuename, new_venueloc, new_venueplace, new_venuecap)
+        return "venue registered"
+    return render_template('new_venue.html')
+
+
+
+print(datetime.datetime.utcnow)
+
+
+
+
 
 
 
